9497b9b8ef478497caab5d6701634cc4242329ba
MikeCodesSomething <129166471+MikeCodesSomething@users.noreply.github.com>
2023-07-03 14:23:35 +0100

    Add console only functionality for a 5 round game

---
+++ script.js
@@ -0,0 +1,99 @@
+//Script
+let totalRounds = 5
+game()
+
+//Functions
+function game() {
+    let playerScore = 0
+    let computerScore = 0
+    for (let round = 1; round <= totalRounds; round++) {
+        console.log(`Round ${round} / ${totalRounds}, the score is Player: ${playerScore}, Computer: ${computerScore}`)
+        computerChoice = getComputerChoice()
+        playerChoice = getPlayerChoice()
+        let result = playRound(computerChoice,playerChoice)
+        if (result === 'playerWin') {
+            playerScore++
+        }
+        if (result === 'computerWin') {
+            computerScore++
+        }
+    }
+    console.log(`Final scores: Player: ${playerScore}, Computer: ${computerScore}`)   
+}
+
+function playRound(computerChoice,playerChoice) {
+    if (playerChoice === computerChoice) {
+        console.log('Draw!')
+        return 'draw'
+    }
+    else if (playerChoice === 'rock') {
+        if (computerChoice === 'scissors') {
+            console.log(`You win! ${playerChoice} beats ${computerChoice}.`)
+            return 'playerWin'
+        }
+        else if (computerChoice === 'paper') {
+            console.log(`You lose! ${computerChoice} beats ${playerChoice}.`)
+            return 'computerWin'
+        }
+
+    }
+    else if (playerChoice === 'scissors') {
+        if (computerChoice === 'paper') {
+            console.log(`You win! ${playerChoice} beats ${computerChoice}.`)
+            return 'playerWin'
+        }
+        else if (computerChoice === 'rock') {
+            console.log(`You lose! ${computerChoice} beats ${playerChoice}.`)
+            return 'computerWin'
+
+        }
+
+    }
+    else if (playerChoice === 'paper') {
+        if (computerChoice === 'rock') {
+            console.log(`You win! ${playerChoice} beats ${computerChoice}.`)
+            return 'playerWin'
+        }
+        else if (computerChoice === 'scissors') {
+            console.log(`You lose! ${computerChoice} beats ${playerChoice}.`)
+            return 'computerWin'
+        }
+
+    }
+
+    else console.log('something is wrong in the playRound function')
+       
+}
+
+
+function getComputerChoice() {
+    let randomValue = Math.floor(Math.random() * 3)
+    if(randomValue === 0) {
+        return 'rock'
+    }
+    else if (randomValue === 1) {
+        return 'paper'
+    }
+    else if (randomValue === 2) {
+        return 'scissors'
+    }
+    else {
+        console.log('something is wrong in getComputerChoice')
+    }
+
+}
+
+function getPlayerChoice() {
+    choice = prompt('Choose rock, paper or scissors (lower case)')
+    if (choice === 'rock' || choice === 'paper' || choice === 'scissors') {
+        return choice
+    }
+    else while(true) {
+        choice = prompt('Try again, your choice must be rock, paper or scissors in lower case')
+        if (choice === 'rock' || choice === 'paper' || choice === 'scissors') {
+            return choice
+    }
+    console.log('something is wrong in the getPlayerChoice function')
+    }
+}
+
